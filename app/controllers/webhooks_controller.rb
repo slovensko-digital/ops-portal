de80c693419e8ff461df66b2b9348e77b0c05ee6
Marek Ceľuch <celuchmarek@gmail.com>
2024-11-06 12:06:30 +0100

    webhook x hub auth

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -1,7 +1,6 @@
 class WebhooksController < ActionController::API
   before_action :authenticate
   before_action :set_issue, only: %i[ ticket_updated ]
-  # TODO: authentication
 
   def ticket_updated
     @issue.state = ticket_params[:state]
@@ -11,8 +10,7 @@ class WebhooksController < ActionController::API
   private
 
   def set_issue
-    @issue = Issue.find_by(triage_external_id: ticket_params[:id])
-    render :not_found unless @issue
+    @issue = Issue.find_by!(triage_external_id: ticket_params[:id])
   end
 
   def ticket_params
@@ -21,13 +19,12 @@ class WebhooksController < ActionController::API
 
   def authenticate
     sig_header = x_hub_signature_header
-    sign = OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new("sha1"), ENV.fetch("WEBHOOK_SECRET").encode("ASCII"), request.body.read.encode("ASCII"))
-
-    raise BaseError::InvalidRequest.new("Invalid Signature") unless sign == sig_header
+    signature = OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new("sha1"), ENV.fetch("WEBHOOK_SECRET"), request.body.read)
+    raise ActionController::BadRequest.new("Invalid Signature") unless signature == sig_header
   end
 
   def x_hub_signature_header
     raise ActionController::ParameterMissing.new("X-Hub-Signature header") unless request.headers["X-Hub-Signature"]
-    request.headers["X-Hub-Signature"].slice("sha1=")
+    request.headers["X-Hub-Signature"].gsub("sha1=", "")
   end
 end
