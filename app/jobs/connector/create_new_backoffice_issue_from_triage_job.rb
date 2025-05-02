35e86fd5dedeab4e58540cb5a37d294cb4dead85
luciajanikova <19lucia99@gmail.com>
2025-05-02 16:50:25 +0200

    Update Connector::CreateNewBackofficeIssueFromTriageJob

--- app/jobs/connector/create_new_backoffice_issue_from_triage_job.rb
+++ app/jobs/connector/create_new_backoffice_issue_from_triage_job.rb
@@ -22,7 +22,7 @@ class Connector::CreateNewBackofficeIssueFromTriageJob < ApplicationJob
     if import
       zammad_client.check_import_mode! if import
       zammad_group = zammad_api_client::IMPORT_GROUP
-      backoffice_state = ISSUE_STATE_TO_PROCESS_TYPE.fetch(issue_data["ops_state"])
+      backoffice_state = ISSUE_OPS_STATE_TO_BACKOFFICE_STATE.fetch(issue_data["ops_state"])
     else
       backoffice_state = zammad_api_client::DEFAULT_STATE
       zammad_group = zammad_api_client::DEFAULT_GROUP
@@ -37,7 +37,7 @@ class Connector::CreateNewBackofficeIssueFromTriageJob < ApplicationJob
     end
   end
 
-  ISSUE_STATE_TO_PROCESS_TYPE = {
+  ISSUE_OPS_STATE_TO_BACKOFFICE_STATE = {
     "sent_to_responsible" => "new",
     "in_progress" => "open",
     "referred" => "open",
