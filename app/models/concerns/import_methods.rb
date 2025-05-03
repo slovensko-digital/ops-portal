3f75d47c5dfe7d91d81e5c45e170c6338d0d2875
luciajanikova <19lucia99@gmail.com>
2025-05-03 21:54:52 +0200

    Lint

--- app/models/concerns/import_methods.rb
+++ app/models/concerns/import_methods.rb
@@ -19,26 +19,26 @@ module ImportMethods
 
     def attachment_mimetype_by_name(name)
       case File.extname(name.to_s).downcase
-      when '.pdf'
-        'application/pdf'
-      when '.xml'
-        'application/xml'
-      when '.zip'
-        'application/x-zip-compressed'
-      when '.txt'
-        'text/plain'
-      when '.doc'
-        'application/msword'
-      when '.docx'
-        'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
-      when '.jpg', '.jpeg'
-        'image/jpeg'
-      when '.png'
-        'image/png'
-      when '.tiff', '.tif'
-        'image/tiff'
+      when ".pdf"
+        "application/pdf"
+      when ".xml"
+        "application/xml"
+      when ".zip"
+        "application/x-zip-compressed"
+      when ".txt"
+        "text/plain"
+      when ".doc"
+        "application/msword"
+      when ".docx"
+        "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+      when ".jpg", ".jpeg"
+        "image/jpeg"
+      when ".png"
+        "image/png"
+      when ".tiff", ".tif"
+        "image/tiff"
       else
-        'application/octet-stream'
+        "application/octet-stream"
       end
     end
   end
