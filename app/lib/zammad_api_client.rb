1e987d341314d46bfc255d3b1e04d62a62a65fbb
luciajanikova <19lucia99@gmail.com>
2025-05-03 23:34:35 +0200

    Do not send emails to zammad & user only users display_name

--- app/lib/zammad_api_client.rb
+++ app/lib/zammad_api_client.rb
@@ -254,19 +254,13 @@ class ZammadApiClient
     begin
       # TODO what if there is existing non-portal user?
       zammad_user = @client.user.create(
-        firstname: user.firstname,
-        lastname: user.lastname,
-        email: user.email,
+        firstname: user.display_name,
         roles: [ "Portal User" ],
         origin: "portal"
       )
       zammad_user.id
     rescue RuntimeError => e
-      raise e unless e.message.include? "is already used for another user."
-
-      result = find_zammad_user(user.email)
-      raise "Can't find nor create triage zammad user with email: #{user.email}" unless result
-      result.id
+      raise e
     end
   end
 
@@ -274,17 +268,12 @@ class ZammadApiClient
     begin
       zammad_user = @client.user.create(
         firstname: user.firstname,
-        lastname: user.lastname,
-        email: user.email,
+        lastname: user.lastname, # TODO pri agentoch je ok posielat do triage zammadu aj lastname ?
         roles: [ "Agent" ]
       )
       zammad_user.id
     rescue RuntimeError => e
-      raise e unless e.message.include? "is already used for another user."
-
-      result = find_zammad_user(user.email)
-      raise "Can't find nor create triage zammad user with email: #{user.email}" unless result
-      result.id
+      raise e
     end
   end
 
@@ -299,7 +288,7 @@ class ZammadApiClient
       raise e unless e.message.include? "is already used for another user."
 
       result = find_zammad_user(responsible_subject.subject_name)
-      raise "Can't create triage zammad user for responsible subject email: #{responsible_subject.subject_name}" unless result
+      raise "Can't create triage zammad user for responsible subject: #{responsible_subject.subject_name}" unless result
       result.id
     end
   end
