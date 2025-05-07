1069a40c66aa4e82c575ad957aef7f389871f10d
Jan Suchal <jan.suchal@gmail.com>
2025-05-07 11:41:56 +0200

    Fix for imported users

--- app/models/legacy/user.rb
+++ app/models/legacy/user.rb
@@ -69,7 +69,7 @@ module Legacy
         city_id: legacy_record.cityid,
         municipality: ::Municipality.find_by(legacy_id: legacy_record.mesto),
         street: ::Street.find_by(legacy_id: legacy_record.streetid),
-        onboarded: true,
+        onboarded: true
       }
     end
 
