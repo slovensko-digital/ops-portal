45d516e79c738e9c9036ad3732abb251fae26e98
Marek Ceľuch <celuchmarek@gmail.com>
2025-05-07 22:43:08 +0200

    Update app/jobs/sync_issue_activities_to_triage_job.rb
    Co-authored-by: Jano Suchal <jan.suchal@slovensko.digital>

--- app/jobs/sync_issue_activities_to_triage_job.rb
+++ app/jobs/sync_issue_activities_to_triage_job.rb
@@ -8,10 +8,8 @@ class SyncIssueActivitiesToTriageJob < ApplicationJob
       find_or_create_triage_portal_user!(activity.activity_object.author, client) if activity.activity_object.author && !activity.activity_object.author&.external_id
 
       if issue.triage_process?
-        article_id = client.create_article!(issue.triage_external_id, activity.activity_object, sender: sender_type(activity.activity_object.author))
-      else
-        article_id = client.create_article!(issue.resolution_external_id, activity.activity_object, sender: sender_type(activity.activity_object.author))
-      end
+      external_id = issue.triage_process? ? issue.triage_external_id : issue.resolution_external_id
+      article_id = client.create_article!(external_id, activity.activity_object, sender: sender_type(activity.activity_object.author))
 
       raise unless article_id
 
