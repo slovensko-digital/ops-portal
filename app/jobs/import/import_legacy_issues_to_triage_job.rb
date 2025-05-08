c27b8b0e7fcc7755bf25a15536ae06e9eab5f120
luciajanikova <19lucia99@gmail.com>
2025-05-08 11:58:16 +0200

    Add import jobs for triage & backoffice legacy improt

---
+++ app/jobs/import/import_legacy_issues_to_triage_job.rb
@@ -0,0 +1,14 @@
+module Import
+  class ImportLegacyIssuesToTriageJob < ApplicationJob
+    def perform(municipality:, municipality_district: nil, import_issue_job: ::SyncIssueToTriageJob)
+      Issue
+        .where.not(legacy_id: nil)
+        .where(municipality: municipality)
+        .where(municipality_district: municipality_district).find_in_batches do |group|
+        group.each do |issue|
+          import_issue_job.perform_later(issue, import: true)
+        end
+      end
+    end
+  end
+end
