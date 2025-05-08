4e18904042803167b72af148df248ef16fcd6245
Marek Ceľuch <celuchmarek@gmail.com>
2025-05-08 10:46:47 +0200

    prettier tests

--- test/models/municipality_test.rb
+++ test/models/municipality_test.rb
@@ -2,71 +2,54 @@ require "test_helper"
 
 class MunicipalityTest < ActiveSupport::TestCase
   test "find_by_address returns municipality by municipality when only municipality matches aliases" do
-    municipality = municipalities(:two)
-
-    result_municipality, result_district = Municipality.find_by_address(
+    assert_equal [ municipalities(:banska_bystrica), nil ], Municipality.find_by_address(
       city: "Irrelevant",
       municipality: "banska-bystrica",
       suburb: "Irrelevant"
     )
-
-    assert_equal municipality, result_municipality
-    assert_nil result_district
   end
 
   test "find_by_address returns municipality district when suburb matches district aliases" do
-    municipality = municipalities(:one)
-    district = municipality_districts(:one)
+    municipality = municipalities(:bratislava)
+    district = municipality_districts(:stare_mesto_ba)
 
-    result_municipality, result_district = Municipality.find_by_address(
+    assert_equal [ municipality, district ], Municipality.find_by_address(
       city: "Irrelevant",
       municipality: "Bratislava",
       suburb: "stare-mesto"
     )
-
-    assert_equal municipality, result_municipality
-    assert_equal district, result_district
   end
 
   test "find_by_address prioritizes district from Bratislava when multiple districts match" do
-    bratislava = municipalities(:one)
-    district = municipality_districts(:one)
+    municipality = municipalities(:bratislava)
+    district = municipality_districts(:stare_mesto_ba)
 
-    result_municipality, result_district = Municipality.find_by_address(
+    assert_equal [ municipality, district ], Municipality.find_by_address(
       city: "Bratislava",
       municipality: "Staré Mesto",
       suburb: "Irrelevant"
     )
-
-    assert_equal bratislava, result_municipality
-    assert_equal district, result_district
-  end
+ end
 
   test "find_by_address returns nil when municipality exists but has no aliases" do
-    municipality = municipalities(:one)
+    municipality = municipalities(:bratislava)
     municipality.update!(aliases: [])
 
-    result_municipality, result_district = Municipality.find_by_address(
+    assert_equal [ nil, nil ], Municipality.find_by_address(
       city: "Irrelevant",
       municipality: "Bratislava",
       suburb: "Irrelevant"
     )
-
-    assert_nil result_municipality
-    assert_nil result_district
   end
 
   test "find_by_address returns nil, nil when municipality exists but is not active" do
-    municipality = municipalities(:two)
+    municipality = municipalities(:banska_bystrica)
     municipality.update!(active: false)
 
-    result_municipality, result_district = Municipality.find_by_address(
+    assert_equal [ nil, nil ], Municipality.find_by_address(
       city: "Irrelevant",
       municipality: "banska-bystrica",
       suburb: "Irrelevant"
     )
-
-    assert_nil result_municipality
-    assert_nil result_district
   end
 end
