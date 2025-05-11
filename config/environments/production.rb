ffd314359fee84c7222dbba2d3103a2a1288a475
Marek Ceľuch <celuchmarek@gmail.com>
2025-05-11 20:22:25 +0200

    add config.action_mailer.asset_host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,6 +69,8 @@ Rails.application.configure do
     authentication: :plain
   }
 
+  config.action_mailer.asset_host = "https://#{ENV.fetch("APP_HOST")}"
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
