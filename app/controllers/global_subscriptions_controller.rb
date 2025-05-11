aa8547fd96b8c76ffe8399f71308fb89cd6b2a0e
Marek Ceľuch <celuchmarek@gmail.com>
2025-05-11 16:48:05 +0200

    minor cleanup

--- app/controllers/global_subscriptions_controller.rb
+++ app/controllers/global_subscriptions_controller.rb
@@ -21,14 +21,4 @@ class GlobalSubscriptionsController < ApplicationController
       head :not_found
     end
   end
-
-  def subscription
-    # TODO: add custom views for subscription unsubscribes
-    subscription = IssueSubscription.find_by(email_unsubscribe_token: params[:token])
-    if subscription && subscription.destroy
-      redirect_to root_path, notice: "Odhlásenie z odberu bolo úspešné."
-    else
-      redirect_to root_path, alert: "Odhlásenie z odberu sa nepodarilo. Skontrolujte prosím platnosť odkazu."
-    end
-  end
 end
