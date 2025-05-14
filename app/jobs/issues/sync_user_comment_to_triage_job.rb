eb0f223b09b939c89552381ac4cd407be172e045
luciajanikova <19lucia99@gmail.com>
2025-05-14 15:18:04 +0200

    Remove TODO

--- app/jobs/issues/sync_user_comment_to_triage_job.rb
+++ app/jobs/issues/sync_user_comment_to_triage_job.rb
@@ -2,7 +2,6 @@ class Issues::SyncUserCommentToTriageJob < ApplicationJob
   queue_as :default
 
   def perform(user_comment)
-    # TODO make there are no other editable user_comments
     if user_comment.within_editing_window?
       self.class.set(wait_until: user_comment.editing_window_end).perform_later(user_comment)
       return
