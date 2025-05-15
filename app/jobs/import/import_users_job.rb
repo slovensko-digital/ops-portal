90d9fbcced77bdbca1f88978c05044cd55780c59
luciajanikova <19lucia99@gmail.com>
2025-05-15 17:53:48 +0200

    Include ImportMethods

--- app/jobs/import/import_users_job.rb
+++ app/jobs/import/import_users_job.rb
@@ -2,6 +2,8 @@ module Import
   class ImportUsersJob < ApplicationJob
     queue_with_priority 100
 
+    include ImportMethods
+
     def perform
       Legacy::OldUser.where(rights: "U").find_each do |legacy_record|
         user = Legacy::User.create_user_from_legacy_record(legacy_record)
