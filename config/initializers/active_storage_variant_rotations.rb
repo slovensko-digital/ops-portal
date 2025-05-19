980f85487394d87e50109ff8e61cb5cf7aef5d7b
Jan Suchal <jan.suchal@gmail.com>
2025-05-19 22:15:28 +0200

    OMG

--- config/initializers/active_storage_variant_rotations.rb
+++ config/initializers/active_storage_variant_rotations.rb
@@ -5,5 +5,22 @@ Rails.application.config.to_prepare do
       transformations[:rotate] = blob.rotation if blob.rotation
       blob.variant(transformations)
     end
+
+    def representation(transformations)
+      transformations = transformations_by_name(transformations)
+      transformations[:rotate] = blob.rotation if blob.rotation
+      blob.representation(transformations)
+    end
+  end
+
+  module ActiveStorage::Blob::Representable
+    def variant(transformations)
+      if variable?
+        transformations[:rotate] = rotation if self.rotation
+        variant_class.new(self, ActiveStorage::Variation.wrap(transformations).default_to(default_variant_transformations))
+      else
+        raise ActiveStorage::InvariableError, "Can't transform blob with ID=#{id} and content_type=#{content_type}"
+      end
+    end
   end
 end
