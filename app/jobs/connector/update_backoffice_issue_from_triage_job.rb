a93e66c8ab6abb53d861c64bcb563ea73c1c4586
Marek Ceľuch <celuchmarek@gmail.com>
2025-05-20 10:33:44 +0200

    minimal diff

--- app/jobs/connector/update_backoffice_issue_from_triage_job.rb
+++ app/jobs/connector/update_backoffice_issue_from_triage_job.rb
@@ -2,7 +2,6 @@ class Connector::UpdateBackofficeIssueFromTriageJob < ApplicationJob
   def perform(tenant, issue_id, zammad_client: Connector::BackofficeZammadEnvironment.client(tenant), ops_api_client: Connector::OpsApiClient)
     ops_client = ops_api_client.new(tenant)
 
-
     issue_data = ops_client.get_issue issue_id
     zammad_client.update_issue! issue_id, issue_data
   end
--- app/jobs/connector/update_triage_issue_from_backoffice_job.rb
+++ app/jobs/connector/update_triage_issue_from_backoffice_job.rb
@@ -1,6 +1,6 @@
 class Connector::UpdateTriageIssueFromBackofficeJob < ApplicationJob
-  def perform(tenant, ticket_id, zammad_client: Connector::BackofficeZammadEnvironment.client(tenant), ops_api_client: Connector::OpsApiClient)
-    issue_data = zammad_client.get_issue(ticket_id)
+  def perform(tenant, ticket_id, zammad_api_client: Connector::BackofficeZammadEnvironment.client(tenant), ops_api_client: Connector::OpsApiClient)
+    issue_data = zammad_api_client.get_issue(ticket_id)
     ops_api_client.new(tenant).update_issue(tenant.issues.find_by(backoffice_external_id: ticket_id).triage_external_id, issue_data)
   end
 end
