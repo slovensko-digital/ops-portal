a1f49652d827a0cb0ba9c0899290c433150846e2
Marek Ceľuch <celuchmarek@gmail.com>
2025-06-03 13:40:24 +0200

    perform now

--- app/jobs/triage/update_portal_activity_from_triage_job.rb
+++ app/jobs/triage/update_portal_activity_from_triage_job.rb
@@ -1,7 +1,7 @@
 class Triage::UpdatePortalActivityFromTriageJob < ApplicationJob
   def perform(ticket_id, article_id, triage_zammad_client: TriageZammadEnvironment.client)
     comment = Issues::Comment.find_by(triage_external_id: article_id.to_i)
-    return CreateNewPortalActivityFromTriageJob.perform_later(ticket_id, article_id) unless comment
+    return CreateNewPortalActivityFromTriageJob.perform_now(ticket_id, article_id, triage_zammad_client: triage_zammad_client) unless comment
 
     _ticket, article = triage_zammad_client.find_article(ticket_id, article_id)
     return Rails.logger.info("Article ticket_id: #{ticket_id}, article_id: #{article_id} not found") unless article
