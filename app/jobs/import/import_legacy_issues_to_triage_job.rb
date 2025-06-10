5c22745a956df7f6c24be832945ee8687767948d
luciajanikova <19lucia99@gmail.com>
2025-06-10 09:07:03 +0200

    Fix Import::ImportLegacyIssuesToTriageJob query

--- app/jobs/import/import_legacy_issues_to_triage_job.rb
+++ app/jobs/import/import_legacy_issues_to_triage_job.rb
@@ -3,10 +3,14 @@ module Import
     queue_with_priority 100
 
     def perform(municipality:, municipality_district: nil, import_issue_job: ::SyncIssueToTriageJob)
+      conditions = {
+        municipality: municipality,
+      }
+      conditions.merge!(municipality_district: municipality_district) if municipality_district
+
       Issue
         .where.not(legacy_id: nil)
-        .where(municipality: municipality)
-        .where(municipality_district: municipality_district).find_in_batches do |group|
+        .where(**conditions).find_in_batches do |group|
         group.each do |issue|
           import_issue_job.perform_later(issue, import: true)
         end
