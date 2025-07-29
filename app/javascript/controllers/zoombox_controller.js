eec618ed96dbd4a4f7738ea4f95291f151c190d7
Richard Lences <richardlences0@gmail.com>
2025-07-29 16:25:22 +0200

    add arrows to zoombox

--- app/javascript/controllers/zoombox_controller.js
+++ app/javascript/controllers/zoombox_controller.js
@@ -3,9 +3,74 @@ import Zoombox from "@stimulus-components/dialog"
 export default class extends Zoombox {
   static targets = ["dialog", "image"]
 
+  connect() {
+    super.connect();
+    this.currentIndex = 0;
+    this.images = Array.from(document.querySelectorAll('[data-zoombox-img-src-param]'))
+      .map(element => ({
+        src: element.dataset.zoomboxImgSrcParam,
+        index: parseInt(element.dataset.zoomboxImgIndexParam)
+      }));
+    this.handleKeydown = this.handleKeydown.bind(this);
+  }
+
   open(event) {
+    this.currentIndex = parseInt(event.params.imgIndex);
     this.imageTarget.src = ""; // prevent flickering
     this.imageTarget.src = event.params.imgSrc;
     super.open(event);
+    this.updateArrows();
+    document.addEventListener('keydown', this.handleKeydown);
+  }
+
+  close(event) {
+    super.close(event);
+    document.removeEventListener('keydown', this.handleKeydown);
+  }
+
+  handleKeydown(event) {
+    if (event.key === 'ArrowLeft') {
+      event.preventDefault();
+      this.prevImage(event);
+    } else if (event.key === 'ArrowRight') {
+      event.preventDefault();
+      this.nextImage(event);
+    }
+  }
+
+  prevImage(event) {
+    event.stopPropagation();
+    if (this.currentIndex > 0) {
+      this.currentIndex--;
+      this.updateImage();
+    }
+  }
+
+  nextImage(event) {
+    event.stopPropagation();
+    if (this.currentIndex < this.images.length - 1) {
+      this.currentIndex++;
+      this.updateImage();
+    }
+  }
+
+  updateImage() {
+    const image = this.images.find(img => img.index === this.currentIndex);
+    if (image) {
+      this.imageTarget.src = "";
+      this.imageTarget.src = image.src;
+      this.updateArrows();
+    }
+  }
+
+  updateArrows() {
+    const prevButton = this.dialogTarget.querySelector('.left-arrow');
+    const nextButton = this.dialogTarget.querySelector('.right-arrow');
+
+    prevButton.disabled = this.currentIndex === 0;
+    nextButton.disabled = this.currentIndex === this.images.length - 1;
+
+    prevButton.style.opacity = this.currentIndex === 0 ? '0.3' : '1';
+    nextButton.style.opacity = this.currentIndex === this.images.length - 1 ? '0.3' : '1';
   }
 }
