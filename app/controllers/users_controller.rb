b9cb56755e936105c84048897745a740d0e37991
Jozef Steinhübl <contact@xhyrom.dev>
2025-10-02 17:36:27 +0200

    feat: add support for viewing other users' profiles & stats

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
   def show
+    if params[:id].to_i == current_user.id
+      redirect_to profile_path and return
+    end
+
     @user = User.where(anonymous: false).find(params[:id])
+    @issues = @user.issues.publicly_visible.newest.page(params[:page]).per(8)
   end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,17 @@
 module UsersHelper
+  def user_subject(user, avatar_variant: :normal)
+    avatar = content_tag(:figure) do
+      if user.anonymous?
+        content_tag(:picture) { user_avatar(user, variant: avatar_variant) }
+      else
+        link_to "/pouzivatelia/#{user.id}", data: { turbo: false } do
+          content_tag(:picture) { user_avatar(user, variant: avatar_variant) }
+        end
+      end
+    end
+
+    name = content_tag(:div, user.present? ? user.display_name : "Neznámy autor", class: "name")
+
+    safe_join([ avatar, name ])
+  end
 end
