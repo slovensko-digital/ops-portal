830d3c343cad32a4f0a5341f9339d1d84d01cfdf
Jozef Steinhübl <contact@xhyrom.dev>
2025-10-09 21:23:54 +0200

    refactor(UsersController): use before action & rescue from

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,25 @@
 class UsersController < ApplicationController
+  before_action :redirect_if_self, only: :show
+  before_action :set_user, only: :show
+  before_action only: :show do
+    render :anonymous, status: :forbidden if @user.anonymous?
+  end
+
+  rescue_from ActiveRecord::RecordNotFound, with: -> { render :not_found, status: :not_found }
+
   def show
+    @issues = @user.issues.publicly_visible.newest.page(params[:page]).per(8)
+  end
+
+  def redirect_if_self
+    return if current_user.is_a?(AnonymousUser)
+
     if params[:id].to_i == current_user.id
-      redirect_to profile_path and return
+      redirect_to profile_path
     end
+  end
 
-    begin
-      @user = User.find(params[:id])
-
-      if @user.anonymous?
-        render :anonymous, status: :forbidden and return
-      end
-
-      @issues = @user.issues.publicly_visible.newest.page(params[:page]).per(8)
-    rescue ActiveRecord::RecordNotFound
-      render :not_found, status: :not_found
-    end
+  def set_user
+    @user = User.find(params[:id])
   end
 end
