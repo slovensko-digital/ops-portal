27221576462854fdf34c759d66c57989c70db74b
Marek Ceľuch <celuchmarek@gmail.com>
2025-10-28 09:27:25 +0100

    Update db/migrate/20251020135223_add_display_date_index_to_issues.rb
    Co-authored-by: Jano Suchal <jan.suchal@slovensko.digital>

--- db/migrate/20251020135223_add_display_date_index_to_issues.rb
+++ db/migrate/20251020135223_add_display_date_index_to_issues.rb
@@ -1,5 +1,5 @@
 class AddDisplayDateIndexToIssues < ActiveRecord::Migration[8.0]
   def change
-    add_index :issues, "COALESCE(resolution_started_at, created_at) DESC", name: "index_issues_on_display_date"
+    add_index :issues, "COALESCE(resolution_started_at, created_at) DESC", name: "index_issues_on_effective_date"
   end
 end
