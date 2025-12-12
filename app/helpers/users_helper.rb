27bb8a74e030de3d6aa1c8604ab50481ca44ac7e
Jozef Steinhübl <contact@xhyrom.dev>
2025-12-12 15:53:05 +0100

    fix: undefined method xxxx_path in link_to_user_profile

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,7 @@
 module UsersHelper
-  def link_to_user_profile(user, avatar_variant: :normal)
+  def link_to_user_profile(user, display_name: nil, avatar_variant: :normal)
     avatar = content_tag(:figure) do
-      if user.anonymous?
+      if not user.is_a?(User) or user.anonymous?
         content_tag(:picture) { user_avatar(user, variant: avatar_variant) }
       else
         link_to user, data: { turbo_frame: "_top" } do
@@ -10,7 +10,7 @@ module UsersHelper
       end
     end
 
-    name = content_tag(:div, user.present? ? user.display_name : "Neznámy autor", class: "name")
+    name = content_tag(:div, display_name.presence || user&.display_name || "Neznámy autor", class: "name")
 
     safe_join([ avatar, name ])
   end
