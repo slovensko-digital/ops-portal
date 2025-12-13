d854881bf9ad1840ee34dc3d0a599d6e1f1a5dad
Jozef Steinhübl <contact@xhyrom.dev>
2025-12-13 16:04:35 +0100

    refactor: separate link_to_comment_author_profile

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,7 @@
 module UsersHelper
-  def link_to_user_profile(user, display_name: nil, avatar_variant: :normal)
+  def link_to_user_profile(user, avatar_variant: :normal)
     avatar = content_tag(:figure) do
-      if not user.is_a?(User) or user.anonymous?
+      if user.anonymous?
         content_tag(:picture) { user_avatar(user, variant: avatar_variant) }
       else
         link_to user, data: { turbo_frame: "_top" } do
@@ -10,8 +10,20 @@ module UsersHelper
       end
     end
 
-    name = content_tag(:div, display_name.presence || user&.display_name || "Neznámy autor", class: "name")
+    name = content_tag(:div, user.present? ? user.display_name : "Neznámy autor", class: "name")
 
     safe_join([ avatar, name ])
+  end
+
+  def link_to_comment_author_profile(comment, avatar_variant: :normal)
+    if comment.author.is_a?(User)
+      return link_to_user_profile(comment.author, avatar_variant: avatar_variant)
+    end
+
+    avatar = content_tag(:figure) do
+      content_tag(:picture) { user_avatar(comment.author, variant: avatar_variant) }
+    end
+
+    safe_join([ avatar, comment.author_display_name ])
   end
 end
