a214b9b3147cd8c869ac054579c00f02261de9aa
Jan Suchal <jan.suchal@gmail.com>
2025-12-26 14:22:40 +0100

    Simplify user stat calculation

--- app/jobs/user_stats/refresh_counts_all_job.rb
+++ app/jobs/user_stats/refresh_counts_all_job.rb
@@ -3,36 +3,26 @@ class UserStats::RefreshCountsAllJob < ApplicationJob
 
   def perform
     sql = <<-SQL
-      WITH stats AS (
-        SELECT
-          u.id AS user_id,
-          COALESCE(COUNT(DISTINCT i.id), 0) AS total_issues,
-          COALESCE(COUNT(DISTINCT c.id), 0) AS total_comments,
-          COALESCE(COUNT(DISTINCT iu.id), 0) AS total_verified_issues
-        FROM
-          users u
-        LEFT JOIN
-          issues i ON u.id = i.author_id AND i.state_id NOT IN (
-            SELECT id FROM issues_states WHERE key IN (?)
-          )
-        LEFT JOIN
-          issues_comments c ON u.id = c.user_author_id
-        LEFT JOIN
-          issues_updates iu ON u.id = iu.author_id AND iu.verification_status = 1
-        GROUP BY
-          u.id
-      )
-      UPDATE
-        users
+      UPDATE users
       SET
-        stats_issues_count = stats.total_issues,
-        stats_comments_count = stats.total_comments,
-        stats_verified_issues_count = stats.total_verified_issues,
+        stats_issues_count = (
+          SELECT COUNT(*)
+          FROM issues
+          WHERE author_id = users.id
+            AND state_id NOT IN (SELECT id FROM issues_states WHERE key IN (?))
+        ),
+        stats_comments_count = (
+          SELECT COUNT(*)
+          FROM issues_comments
+          WHERE user_author_id = users.id
+        ),
+        stats_verified_issues_count = (
+          SELECT COUNT(*)
+          FROM issues_updates
+          WHERE author_id = users.id
+            AND verification_status = 1
+        ),
         updated_at = CURRENT_TIMESTAMP
-      FROM
-        stats
-      WHERE
-        users.id = stats.user_id;
     SQL
 
     ActiveRecord::Base.connection.execute(ActiveRecord::Base.sanitize_sql_array([ sql, Issues::State::PRIVATE_KEYS ]))
