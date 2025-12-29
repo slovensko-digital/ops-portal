6fdbdc365b548ced51b8efe78d7320c4ff3d168f
Jozef Steinhübl <contact@xhyrom.dev>
2025-12-29 16:34:29 +0100

    refactor: destroy draft on issue deletion

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -63,7 +63,7 @@ class Issue < ApplicationRecord
   belongs_to :state, class_name: "Issues::State", optional: true
   belongs_to :archived_state, class_name: "Issues::State", optional: true
 
-  has_one :draft, class_name: "Issues::Draft", dependent: :nullify
+  has_one :draft, class_name: "Issues::Draft", dependent: :destroy
 
   has_many :activities, class_name: "Issues::Activity", dependent: :destroy
   has_many :comment_activities, class_name: "Issues::CommentActivity", dependent: :destroy
