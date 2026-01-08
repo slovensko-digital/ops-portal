ccc0e8af3c90f9555fd6a59f575d9057f0d06813
Marek Ceľuch <celuchmarek@gmail.com>
2026-01-08 09:37:44 +0100

    do not transform non representable photos

--- app/helpers/responsive_helper.rb
+++ app/helpers/responsive_helper.rb
@@ -1,5 +1,7 @@
 module ResponsiveHelper
   def responsive_image_tag(attachment, srcset:, **options)
+    return nil unless attachment.representable?
+
     srcset2 = srcset.map.with_index(1) do |variant, idx|
       "#{url_for(attachment.variant(variant))} #{idx}x"
     end.join(", ")
