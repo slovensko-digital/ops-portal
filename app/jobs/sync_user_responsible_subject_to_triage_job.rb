d65d4513c29351887cb7fd2da1afd4b6b2bbf489
Jozef Steinhübl <contact@xhyrom.dev>
2026-01-29 20:39:36 +0100

    fix: access responsible subject from user

--- app/jobs/sync_user_responsible_subject_to_triage_job.rb
+++ app/jobs/sync_user_responsible_subject_to_triage_job.rb
@@ -1,6 +1,6 @@
 class SyncUserResponsibleSubjectToTriageJob < ApplicationJob
   def perform(user, client: TriageZammadEnvironment.client)
-    external_id = client.create_responsible_subject!(responsible_subject)
+    external_id = client.create_responsible_subject!(user.responsible_subject)
 
     user.update!(external_id: external_id)
     user.responsible_subject.update!(external_id: external_id)
