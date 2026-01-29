d3b363ee96b4c18bbaf48c1ebdceae427039b82b
Jan Suchal <jan.suchal@gmail.com>
2026-01-29 08:38:14 +0100

    Simplify test

--- test/controllers/legacy/redirects_controller_test.rb
+++ test/controllers/legacy/redirects_controller_test.rb
@@ -2,21 +2,11 @@ require "test_helper"
 
 class Legacy::RedirectsControllerTest < ActionDispatch::IntegrationTest
   test "should redirect legacy user URL to user page for User::Citizen" do
-    citizen = users(:one)
-    citizen.update!(legacy_id: 12345)
+    citizen = users(:legacy_citizen)
 
     get "/r/ludia/#{citizen.legacy_id}"
 
     assert_redirected_to user_path(citizen)
-  end
-
-  test "should redirect legacy user URL to user page for User::ResponsibleSubject" do
-    responsible_subject = users(:responsible_subject)
-    responsible_subject.update!(legacy_id: 67890)
-
-    get "/r/ludia/#{responsible_subject.legacy_id}"
-
-    assert_redirected_to user_path(responsible_subject)
   end
 
   test "should return 404 when user with legacy_id not found" do
