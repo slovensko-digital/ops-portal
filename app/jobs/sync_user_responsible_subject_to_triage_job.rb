4cbd4df0ce5360628b48ccb1cd319aab9cdc35f2
Jozef Steinhübl <contact@xhyrom.dev>
2026-01-30 21:43:36 +0100

    feat: update portal user from triage (create responsible subject if missing)

--- app/jobs/sync_user_responsible_subject_to_triage_job.rb
+++
@@ -1,8 +0,0 @@
-class SyncUserResponsibleSubjectToTriageJob < ApplicationJob
-  def perform(user, client: TriageZammadEnvironment.client)
-    external_id = client.create_responsible_subject!(user.responsible_subject)
-
-    user.update!(external_id: external_id)
-    user.responsible_subject.update!(external_id: external_id)
-  end
-end
