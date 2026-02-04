6ae9ea15fafc253f4b009d32609f7552607328da
Jozef Steinhübl <contact@xhyrom.dev>
2026-02-04 21:32:17 +0100

    feat(Triage::UpdatePortalUserFromTriageJob): unify rs user synchronization from triage

--- app/jobs/triage/update_portal_user_from_triage_job.rb
+++ app/jobs/triage/update_portal_user_from_triage_job.rb
@@ -3,20 +3,38 @@ class Triage::UpdatePortalUserFromTriageJob < ApplicationJob
     triage_user = triage_zammad_client.find_user(user_id)
     return unless triage_user
 
-    if triage_user.roles.include?("Zodpovedný Subjekt") && !User.exists?(external_id: triage_user.id.to_i)
-      rs = ResponsibleSubject.find_by!(external_id: triage_user.id.to_i)
+    rs = if triage_user.organization.present?
+           ResponsibleSubject.find_by(subject_name: triage_user.organization)
+    elsif triage_user.roles.include?("Zodpovedný Subjekt")
+           ResponsibleSubject.find_by(external_id: triage_user.id.to_i)
+    end
 
-      User::ResponsibleSubject.create!(
-        firstname: triage_user.firstname,
-        lastname: triage_user.lastname,
-        email: triage_user.email,
-        external_id: triage_user.id.to_i,
-        responsible_subject: rs,
-        status: :verified,
-        onboarded: true,
-        password: SecureRandom.hex(32),
-        phone_verified: true
-      )
+    user = User.find_by(external_id: triage_user.id.to_i)
+
+    if rs
+      if user
+        user.update!(
+          firstname: triage_user.firstname,
+          lastname: triage_user.lastname,
+          email: triage_user.email,
+          responsible_subject: rs,
+        )
+      else
+        User::ResponsibleSubject.create!(
+          firstname: triage_user.firstname,
+          lastname: triage_user.lastname,
+          email: triage_user.email,
+          external_id: triage_user.id.to_i,
+          responsible_subject: rs,
+          status: :verified,
+          onboarded: true,
+          password: SecureRandom.hex(32),
+          phone_verified: true
+        )
+      end
+    elsif user.responsible_subject
+      user.anonymize!
+      user.update!(active: false, external_id: nil, responsible_subject: nil)
     end
 
     return unless triage_user.origin == "portal"
