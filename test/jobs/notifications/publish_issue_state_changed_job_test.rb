22db88c7d7e1774f1b4af729124e18706d4fa330
luciajanikova <19lucia99@gmail.com>
2026-02-10 13:15:27 +0100

    Lint

--- test/jobs/notifications/publish_issue_state_changed_job_test.rb
+++ test/jobs/notifications/publish_issue_state_changed_job_test.rb
@@ -17,7 +17,7 @@ module Notifications
       @notification_mailer.expect :issue_resolved, @notification_mailer, []
       @notification_mailer.expect :deliver_later, nil
 
-      state_id_change = [Issues::State.find_by(key: "waiting").id, Issues::State.find_by(key: "resolved").id]
+      state_id_change = [ Issues::State.find_by(key: "waiting").id, Issues::State.find_by(key: "resolved").id ]
       Notifications::PublishIssueStateChangedJob.new.perform(@issue, state_id_change: state_id_change, notification_mailer: @notification_mailer)
 
       assert_mock @notification_mailer
--- test/jobs/notifications/publish_new_issue_comment_job_test.rb
+++ test/jobs/notifications/publish_new_issue_comment_job_test.rb
@@ -1,4 +1,4 @@
-require 'test_helper'
+require "test_helper"
 
 class Notifications::PublishNewIssueCommentJobTest < ActiveJob::TestCase
   setup do
@@ -10,13 +10,13 @@ class Notifications::PublishNewIssueCommentJobTest < ActiveJob::TestCase
     @notification_mailer = Minitest::Mock.new
   end
 
-  test 'sends notifications only to active subscribers (except comment author)' do
+  test "sends notifications only to active subscribers (except comment author)" do
     @active_subscription.subscriber.update(email_notifiable: true)
     @inactive_subscription.subscriber.update(email_notifiable: true)
     @another_active_subscription.subscriber.update(email_notifiable: true)
 
     @notification_mailer.expect(:with, @notification_mailer, [], **{ subscription: @another_active_subscription })
-    @notification_mailer.expect(:new_issue_user_comment, @notification_mailer, [@comment])
+    @notification_mailer.expect(:new_issue_user_comment, @notification_mailer, [ @comment ])
     @notification_mailer.expect(:deliver_later, nil)
 
     Notifications::PublishNewIssueCommentJob.new.perform(@comment, notification_mailer: @notification_mailer)
@@ -24,7 +24,7 @@ class Notifications::PublishNewIssueCommentJobTest < ActiveJob::TestCase
     assert_mock @notification_mailer
   end
 
-  test 'does not send notifications to users who are not email notifiable' do
+  test "does not send notifications to users who are not email notifiable" do
     @active_subscription.subscriber.update(email_notifiable: false)
     @another_active_subscription.subscriber.update(email_notifiable: false)
 
