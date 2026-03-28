a0711763aeb209e0ec0e0bf3af8fad579beaea52
Jozef Steinhübl <contact@xhyrom.dev>
2026-03-28 18:24:30 +0100

    refactor: remove propagation controller

--- app/javascript/controllers/propagation_controller.js
+++
@@ -1,8 +0,0 @@
-import { Controller } from "@hotwired/stimulus"
-
-// Connects to data-controller="propagation"
-export default class extends Controller {
-    stop(event) {
-        event.stopPropagation()
-    }
-}
