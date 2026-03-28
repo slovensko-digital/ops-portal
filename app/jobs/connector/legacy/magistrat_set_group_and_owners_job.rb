58b935d13a3392bfe429545dd20c3db0289fbb7d
luciajanikova <19lucia99@gmail.com>
2026-03-28 14:24:32 +0100

    Lint fix

--- app/jobs/connector/legacy/magistrat_set_group_and_owners_job.rb
+++ app/jobs/connector/legacy/magistrat_set_group_and_owners_job.rb
@@ -30,10 +30,10 @@ class Connector::Legacy::MagistratSetGroupAndOwnersJob < ApplicationJob
     return unless legacy_org_units.any?
 
     selected_organization_unit = if issue.backoffice_owner&.organization_unit_id && legacy_org_units.where(id: issue.backoffice_owner.organization_unit_id).any?
-                                   legacy_org_units.find(issue.backoffice_owner.organization_unit_id)
-                                 else
-                                   legacy_org_units.find_by(legacy_id: issue.legacy_data&.fetch("organizational_unit_id")).presence || legacy_org_units.find_by(legacy_id: issue.legacy_data&.fetch("organization_unit_id2"))
-                                 end
+      legacy_org_units.find(issue.backoffice_owner.organization_unit_id)
+    else
+      legacy_org_units.find_by(legacy_id: issue.legacy_data&.fetch("organizational_unit_id")).presence || legacy_org_units.find_by(legacy_id: issue.legacy_data&.fetch("organization_unit_id2"))
+    end
 
     zammad_client.find_or_create_group(selected_organization_unit.name)
   end
