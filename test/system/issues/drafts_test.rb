92d6ed0c3822c13c67f8a77b60794bcef552fa54
Jano Suchal <jan.suchal@gmail.com>
2026-03-28 14:50:40 +0100

    Update test/system/issues/drafts_test.rb

--- test/system/issues/drafts_test.rb
+++ test/system/issues/drafts_test.rb
@@ -158,6 +158,6 @@ class Issues::DraftsTest < ApplicationSystemTestCase
     click_on "Odoslať podnet"
 
     assert_text "Podnet má problém"
-    assert_text "Mestská časť nie je zatiaľ dostupná na Odkaz pre starostu"
+    assert_text "Mestská časť nie je zatiaľ zapojená do portálu Odkaz pre starostu"
   end
 end
