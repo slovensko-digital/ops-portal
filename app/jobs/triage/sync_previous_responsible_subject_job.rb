3a0d14cddee3b3427db7245991b99e54ebf5147e
luciajanikova <19lucia99@gmail.com>
2026-05-25 15:53:59 +0200

    Sync previous RS only for issues in resolution process

--- app/jobs/triage/sync_previous_responsible_subject_job.rb
+++ app/jobs/triage/sync_previous_responsible_subject_job.rb
@@ -1,6 +1,8 @@
 module Triage
   class SyncPreviousResponsibleSubjectJob < ApplicationJob
     def perform(issue, previous_responsible_subject, client: TriageZammadEnvironment.client)
+      return unless issue.resolution_process?
+
       client.sync_previous_responsible_subject!(issue.resolution_external_id, previous_responsible_subject)
     end
   end
