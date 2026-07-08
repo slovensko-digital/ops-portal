239e6ae4164094efdf05f205e28de8d963a0b9cf
Jozef Steinhübl <contact@xhyrom.dev>
2026-07-08 22:42:12 +0200

    fix: add schema info

--- app/models/issues/system_responsible_subject_change.rb
+++ app/models/issues/system_responsible_subject_change.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: issues_system_responsible_subject_changes
+#
+#  id                              :bigint           not null, primary key
+#  hidden                          :boolean          default(FALSE), not null
+#  uuid                            :uuid             not null
+#  created_at                      :datetime         not null
+#  updated_at                      :datetime         not null
+#  activity_id                     :bigint           not null
+#  new_responsible_subject_id      :bigint
+#  previous_responsible_subject_id :bigint
+#
 class Issues::SystemResponsibleSubjectChange < ApplicationRecord
   belongs_to :activity, class_name: "Issues::Activity", dependent: :destroy
   belongs_to :previous_responsible_subject, optional: true, class_name: "::ResponsibleSubject"
